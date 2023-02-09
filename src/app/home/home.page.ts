0fad8173e43555bc248e9ef861e369a1ba86d50d
bypts <byptechnologysolutions@gmail.com>
2023-02-08 23:49:30 -0500

    finish design

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,15 +6,28 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-  features: any[] = [
+  public features: any[] = [
     { id: 1, name: "Add Income", icon: "add-circle", color: "success" },
     { id: 2, name: "Add Expenses", icon: "remove-circle", color: "danger" },
     { id: 3, name: "Transfer", icon: "send", color: "warning" },
     { id: 4, name: "Pay", icon: "card", color: "danger" },
   ];
-
-  balance: number = 200000;
+  public balance: number = 200000;
+  public transactions: any[] = [
+    { id: 1, type: "Add Income", amount: 20000, icon: "add-circle", color: "success", date: "3:00 pm" },
+    { id: 2, type: "Add Income", amount: 20000, icon: "add-circle", color: "success", date: "3:00 pm" },
+    { id: 3, type: "Add Income", amount: 20000, icon: "add-circle", color: "success", date: "3:00 pm" },
+    { id: 1, type: "Add Income", amount: 20000, icon: "add-circle", color: "success", date: "3:00 pm" },
+    { id: 2, type: "Add Income", amount: 20000, icon: "add-circle", color: "success", date: "3:00 pm" },
+    { id: 3, type: "Add Income", amount: 20000, icon: "add-circle", color: "success", date: "3:00 pm" },
+    { id: 1, type: "Add Income", amount: 20000, icon: "add-circle", color: "success", date: "3:00 pm" },
+    { id: 2, type: "Add Income", amount: 20000, icon: "add-circle", color: "success", date: "3:00 pm" },
+    { id: 3, type: "Add Income", amount: 20000, icon: "add-circle", color: "success", date: "3:00 pm" },
+    { id: 1, type: "Add Income", amount: 20000, icon: "add-circle", color: "success", date: "3:00 pm" },
+    { id: 2, type: "Add Income", amount: 20000, icon: "add-circle", color: "success", date: "3:00 pm" },
+    { id: 3, type: "Add Income", amount: 20000, icon: "add-circle", color: "success", date: "3:00 pm" },
+  ];
 
   constructor() {}
-
+  // add, edit, delete, update transactions
 }
